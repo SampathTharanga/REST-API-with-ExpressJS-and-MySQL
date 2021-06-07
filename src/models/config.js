587b91912d7ed028f4e8c8e64057522a95a7b13f
Sampath Tharanga <sampaththaranga13@gmail.com>
2021-06-07 22:34:55 +0530

    database configuration update

--- src/models/config.js
+++ src/models/config.js
@@ -1,2 +1,14 @@
 require('dotenv').config()
 
+const config = {
+    db: {
+        host: process.env.DB_HOST || 'freedb.tech',
+        user: process.env.DB_USER || 'freedbtech_Sampath',
+        password: process.env.DB_PASS || '19910710',
+        database: process.env.DB_NAME || 'freedbtech_studentdb'
+    },
+    listPerPage: process.env.LIST_PER_PAGE || 10
+}
+
+
+module.exports = config
